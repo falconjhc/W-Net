0a76e114e162a0328923a38f72d13920d4dc5507
falconjhc <falconjhc@gmail.com>
2019-01-28 18:03:52 +0800

    20190128-2

--- TrainingScripts_WNet/More/StylePf144_ContentPf64.py
+++ TrainingScripts_WNet/More/StylePf144_ContentPf64.py
@@ -24,8 +24,8 @@ model_log_path_root = '/Data_HDD/Harric/ChineseCharacterExp/'
 #                   1: training from a based model
 input_args = [
 
-    '--training_from_model_dir',
-    '/Data_HDD/Harric/ChineseCharacterExp/tfModels_WNet/checkpoint/Exp20190119-WNet-NonAdaIN_StylePf80_ContentPf64_GenEncDec6-Res7@Lyr3_DisMdy6conv/',
+    #'--training_from_model_dir',
+    #'/Data_HDD/Harric/ChineseCharacterExp/tfModels_WNet/checkpoint/Exp20190119-WNet-NonAdaIN_StylePf80_ContentPf64_GenEncDec6-Res7@Lyr3_DisMdy6conv/',
 
     '--debug_mode','0',
     '--style_input_number','4', # how many style inputs
@@ -40,7 +40,7 @@ input_args = [
 
     '--train_data_augment','1', # translation? rotation?
     '--train_data_augment_flip','1',
-    '--experiment_id','20190119-WNet-NonAdaIN_StylePf80_ContentPf64',# experiment name prefix
+    '--experiment_id','20190128-WNet-NonAdaIN_StylePf144_ContentPf64',# experiment name prefix
     '--experiment_dir','tfModels_WNet/', # model saving location
     '--log_dir','tfLogsNew_WNet_More/',# log file saving location
     '--print_info_seconds','750',
@@ -78,7 +78,7 @@ input_args = [
     # optimizer parameters
     '--init_lr','0.001',
     '--epoch','2500',
-    '--resume_training','0', # 0: training from scratch; 1: training from a pre-trained point
+    '--resume_training','1', # 0: training from scratch; 1: training from a pre-trained point
 
     '--optimization_method','adam',
     '--final_learning_rate_pctg','0.01',
